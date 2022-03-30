69e608dc95c55961cfa787cfd98018764ca5cc5a
Mario Härtwig <mario.haertwig@sbb.ch>
2022-03-30 09:24:54 +0200

    target-release tag (#48)
    * target-release tag
    
    * target-release tag
    
    * target-release tag
    
    * cherry-picking in multiple branches
    
    * naming
    
    * remove obsolete env variable
    
    * echo
    
    * echo
    
    * echo
    
    * branch
    
    * cherry-pick exit code
    
    * continue-on-error
    
    * undo
    
    * another try
    
    * refactor
    
    * fix
    
    * fix env
    
    * no xargs
    
    * "
    
    * update ticket only on failure
    
    * test
    
    * test
    
    * outputs
    
    * needs
    
    * failed_branches
    
    * >> $GITHUB_ENV
    
    * >> $GITHUB_ENV!
    
    * )
    
    * test
    
    * test
    
    * last try
    
    * second last try
    
    * sync with sbb-angular

--- scripts/update-maintenance-issue.ts
+++ scripts/update-maintenance-issue.ts
@@ -1,63 +1,69 @@
 import { Octokit } from 'octokit';
 
+const githubToken = process.env['GITHUB_TOKEN'];
+const pullRequestNumber = parseInt(process.env['PR_NUMBER']!, 10);
+const failedBranches = process.env['FAILED_BRANCHES']!.split(' ').join(', ');
+
 const repoConfig = {
-    owner: 'mhaertwig', //'sbb-design-systems',
-    repo: 'github-workflow-test', // 'sbb-angular',
+  owner: 'mhaertwig',
+  repo: 'github-workflow-test',
 };
 
 const issuePath = {
-    ...repoConfig,
-    issue_number: 43, // TODO
+  ...repoConfig,
+  issue_number: 1346,
 };
 
 const prPath = {
-    ...repoConfig,
-    pull_number: parseInt(process.env['PR_NUMBER']!, 10)
+  ...repoConfig,
+  pull_number: pullRequestNumber,
 };
 
-const githubToken = process.env['GITHUB_TOKEN'];
+class MaintenanceIssueUpdater {
+  constructor(private _octokit: Octokit, private _now: Date) {}
 
-export class MaintenanceIssueUpdater {
-    constructor(private _octokit: Octokit, private _now: Date) {
+  async run() {
+    const issue = await this._octokit.rest.issues.get(issuePath);
+    const pr = await this._octokit.rest.pulls.get(prPath);
+
+    if (!issue.data.body) {
+      throw new Error('Could not load issue body');
     }
 
-    async run() {
-        const issue = await this._octokit.rest.issues.get(issuePath);
-        const pr = await this._octokit.rest.pulls.get(prPath);
-
-        if (!issue.data.body) {
-            throw new Error('Could not load issue body');
-        }
-
-        if (!pr.data.title || !pr.data.html_url) {
-            throw new Error('Could not load pull request');
-        }
-
-        const hint = '**Unable to merge the following pull requests into the maintenance branch**';
-        let openTasks = this._extractOpenTasks(issue.data.body);
-        openTasks.push(`- [ ] [${pr.data.title}](${pr.data.html_url})`)
-        openTasks = [...new Set(openTasks)];
-        const dateInfo = `${this._now.toISOString()}`
-
-        return this._octokit.rest.issues.update({
-            ...issuePath,
-            body: `${hint}\r\n${openTasks.join('\r\n')}\r\n${dateInfo}`,
-        });
+    if (!pr.data.title || !pr.data.html_url) {
+      throw new Error('Could not load pull request');
     }
 
-    private _extractOpenTasks(issueBody: string = ''): string[] {
-        return issueBody
-            .split('\r\n')
-            .filter((line) =>  line.startsWith('- [ ]'));
-    }
+    const hint = '**Unable to cherry-pick the following pull requests**';
+    const dateInfo = `${this._now.toISOString()}`;
+    let openTasks = this._extractOpenTasks(issue.data.body);
+    openTasks = this._addNewTask(
+      openTasks,
+      `- [ ] [${pr.data.title}](${pr.data.html_url}) (Branches: ${failedBranches})`
+    );
+
+    return this._octokit.rest.issues.update({
+      ...issuePath,
+      body: `${hint}\n${openTasks.join('\n')}\n\n${dateInfo}`,
+    });
+  }
+
+  private _extractOpenTasks(issueBody: string = ''): string[] {
+    return issueBody.split('\n').filter((line) => line.startsWith('- [ ]'));
+  }
+
+  private _addNewTask(tasks: string[], newTask: string): string[] {
+    const newTasks = [...tasks, newTask];
+    return [...new Set(newTasks)];
+  }
 }
 
 if (module === require.main) {
-    const maintenanceIssueUpdater = new MaintenanceIssueUpdater(
-        new Octokit({
-            auth: githubToken,
-        }),
-        new Date(),
-    );
-    maintenanceIssueUpdater.run();
+  const maintenanceIssueUpdater = new MaintenanceIssueUpdater(
+    new Octokit({
+      auth: githubToken,
+    }),
+    new Date()
+  );
+  maintenanceIssueUpdater.run();
 }
